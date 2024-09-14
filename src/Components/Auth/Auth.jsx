b1061cae8ebcd995902238e2fc801633558cc4d0
AbelBatalla <abelbatallaf@gmail.com>
2024-09-14 10:15:15 +0200

    more bug fixing

--- src/Components/Auth/Auth.jsx
+++ src/Components/Auth/Auth.jsx
@@ -1,60 +1,61 @@
 import { auth, googleProvider } from "../../config/firebase";
 import {
     createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
     signInWithPopup,
     signOut,
+    sendPasswordResetEmail,
+    sendEmailVerification,
 } from "firebase/auth";
-import { useState } from "react";
-import styles from "./Auth.module.css";
 
-const Auth = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-
-    const signIn = async () => {
-        try {
-            await createUserWithEmailAndPassword(auth, email, password);
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    const signInWithGoogle = async () => {
-        try {
-            await signInWithPopup(auth, googleProvider);
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    const logout = async () => {
-        try {
-            await signOut(auth);
-        } catch (err) {
-            console.error(err);
-        }
-    };
-
-    return (
-        <div>
-            <input
-                name = "email"
-                placeholder="Email..."
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-                name = "password"
-                placeholder="Password..."
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={signIn}> Sign In</button>
-
-            <button onClick={signInWithGoogle}> Sign In With Google</button>
-
-            <button onClick={logout}> Logout </button>
-        </div>
-    );
+export const signUpEmail = async (email, password) => {
+    try {
+        await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+        console.error(err);
+    }
 };
-export default Auth;
+
+export const loginEmail = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+export const loginGoogle = async () => {
+    try {
+        await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+export const logout = async () => {
+    try {
+        await signOut(auth);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+export const passwordReset = async (email) => {
+    try {
+        await sendPasswordResetEmail(auth, email);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+/*
+export const sendEmailVerification = async () => {
+    try {
+        await sendEmailVerification(auth.currentUser, {
+            url: `${window.location.origin}`,
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
+*/
