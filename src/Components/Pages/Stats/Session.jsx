656ee6cd3464db816c9afcbad25c9ada519bc1f2
AbelBatalla <abelbatallaf@gmail.com>
2024-09-15 19:40:50 +0200

    delete session styling

--- src/Components/Pages/Stats/Session.jsx
+++ src/Components/Pages/Stats/Session.jsx
@@ -33,46 +33,61 @@ const Session = ({ session, onDelete }) => {
 
     return (
         <div className={`${styles.session} ${isExpanded ? styles.expanded : ''}`} onClick={toggleExpand}>
-            <div className={styles['base-data']}>
-                <div><strong>Date</strong><div>{date}</div></div>
-                <div><strong>Time</strong><div>{time}</div></div>
-                <div><strong>Difficulty</strong><div>{getDifficultyLabel(session.Difficulty)}</div></div>
-                <div><strong>Average Response Time</strong><div>{(session.SessionAvgResponseTime / 1000).toFixed(3)}s</div></div>
-                <div><strong>Correct Answers</strong><div>{Math.floor(session.SessionCorrectRate)}%</div></div>
-                <div className={styles.icon}>{isExpanded ? <FaTimes /> : <FaAngleDown />}</div>
-                <div className={styles.iconTrash} onClick={(e) => { e.stopPropagation(); onDelete(session.id); }}><FaTrash /></div>
+            <div className={styles.unexpandedSession}>
+                <div className={styles['base-data']}>
+                    <div><strong>Date</strong>
+                        <div>{date}</div>
+                    </div>
+                    <div><strong>Time</strong>
+                        <div>{time}</div>
+                    </div>
+                    <div><strong>Difficulty</strong>
+                        <div>{getDifficultyLabel(session.Difficulty)}</div>
+                    </div>
+                    <div><strong>Average Response Time</strong>
+                        <div>{(session.SessionAvgResponseTime / 1000).toFixed(3)}s</div>
+                    </div>
+                    <div><strong>Correct Answers</strong>
+                        <div>{Math.floor(session.SessionCorrectRate)}%</div>
+                    </div>
+                    <div className={styles.icon}>{isExpanded ? <FaTimes/> : <FaAngleDown/>}</div>
+                </div>
+                <div className={styles.iconTrash} onClick={(e) => {
+                    e.stopPropagation();
+                    onDelete(session.id);
+                }}><FaTrash/></div>
             </div>
             <div className={styles.runs}>
                 {session.runs.map((run, index) => (
                     <div key={index} className={styles.run}>
-                        <p><strong>Run {index + 1}</strong></p>
-                        <p><strong>Average Response Time</strong>
-                            <div>{(run.avgResponseTime / 1000).toFixed(3)}s</div>
-                        </p>
-                        <p><strong>Correct Answers</strong>
-                            <div>{Math.floor(run.correctRate)}%</div>
-                        </p>
-                    </div>
-                ))}
+                    <p><strong>Run {index + 1}</strong></p>
+                            <p><strong>Average Response Time</strong>
+                                <div>{(run.avgResponseTime / 1000).toFixed(3)}s</div>
+                            </p>
+                            <p><strong>Correct Answers</strong>
+                                <div>{Math.floor(run.correctRate)}%</div>
+                            </p>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
-    );
-};
+            );
+            };
 
-Session.propTypes = {
-    session: PropTypes.shape({
-        Date: PropTypes.string.isRequired,
-        Difficulty: PropTypes.number.isRequired,
-        Profile: PropTypes.string.isRequired,
-        SessionAvgResponseTime: PropTypes.number.isRequired,
-        SessionCorrectRate: PropTypes.number.isRequired,
-        UserID: PropTypes.string.isRequired,
-        runs: PropTypes.arrayOf(PropTypes.shape({
+            Session.propTypes = {
+            session: PropTypes.shape({
+            Date: PropTypes.string.isRequired,
+            Difficulty: PropTypes.number.isRequired,
+            Profile: PropTypes.string.isRequired,
+            SessionAvgResponseTime: PropTypes.number.isRequired,
+            SessionCorrectRate: PropTypes.number.isRequired,
+            UserID: PropTypes.string.isRequired,
+            runs: PropTypes.arrayOf(PropTypes.shape({
             avgResponseTime: PropTypes.number.isRequired,
             correctRate: PropTypes.number.isRequired,
         })).isRequired,
-    }).isRequired,
-    onDelete: PropTypes.func.isRequired,
-};
+        }).isRequired,
+            onDelete: PropTypes.func.isRequired,
+        };
 
-export default Session;
+            export default Session;
