0205cf59e452d10d3647ce15ef9a92786921fabb
AbelBatalla <abelbatallaf@gmail.com>
2024-10-07 23:09:15 +0200

    initial home & other changes + small fixes

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import ProfileSelector from "../Profiles/ProfileSelector";
-import ProfileFormModal from "../Modal/ProfileFormModal";
+import styles from "./Home.module.css";
 
 const Home = () => {
     return (
     <>
-        <h1>Home</h1>
-        <ProfileSelector />
-        <ProfileFormModal text={"create a profile"}/>
+        <div className={styles.homepageContent}>
+            <h1 className={styles.homepageTitle}>Home</h1>
+
+            <p className={styles.homepageText}>
+                Improve your mathematical abilities by training the mapping from estimated quantities to Arabic digits.
+                This connection is essential because mathematical thinking involves both an intuitive sense of numbers,
+                known as the Approximate Number System (ANS), and the precise symbolic system of Arabic digits used for
+                arithmetic operations. Research shows that this type of training helps children improve their ability to
+                perform symbolic additions and subtractions, offering benefits beyond regular practice. In particular,
+                it enhances arithmetic abilities by building a more solid foundation in understanding and using numbers.
+            </p>
+
+            <p className={styles.homepageText}>
+                If you’re an educator, parent, researcher, or student interested in enhancing math performance, you or
+                your students/children/participants can play the Digits game for free here.
+            </p>
+        </div>
     </>
-)
+    )
 };
 export default Home;
--- src/Components/Pages/Other.jsx
+++ src/Components/Pages/Other.jsx
@@ -1,109 +1,59 @@
 import React from "react";
+import styles from "./Other.module.css";
 
 const Other = () => {
     return (
         <>
             <h1>Other</h1>
-            <div>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem sunt
-                itaque, perferendis necessitatibus corrupti ea magni voluptatibus est
-                ipsa. Nulla praesentium dolore eos alias incidunt, odio et eaque, sunt
-                vitae corporis animi, itaque architecto. Sunt minima nostrum fugiat
-                quibusdam voluptate dolore illo cupiditate quam odio sequi. Consequatur
-                tempora ullam, ipsam molestias dolor dolores tempore. Molestias, error
-                porro tenetur delectus numquam animi aut quod consequuntur? Dolor sequi
-                quisquam ex, eaque explicabo, labore maxime veniam voluptas enim
-                recusandae architecto est voluptates optio libero officiis consectetur
-                possimus voluptate ipsam alias velit veritatis reprehenderit
-                repudiandae. Ullam praesentium inventore minima eius dolorem omnis fugit
-                facilis.
-            </div>
-            <br />
-            <div>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-                libero laboriosam quia totam, ducimus dolorum velit voluptates ipsam ad
-                labore repellendus! Reprehenderit corporis labore totam est quas natus
-                non quis? Atque porro obcaecati error hic perspiciatis itaque
-                reprehenderit ut aperiam, ipsa, repellendus voluptatum, architecto sint
-                delectus. Dolorem, officia nostrum? Quae similique eveniet, animi
-                laborum impedit commodi, deleniti cum adipisci atque quidem culpa
-                incidunt placeat id rem dolorum sit eligendi sapiente ea sed, voluptate
-                voluptates minima pariatur ipsam delectus. Inventore deserunt
-                exercitationem repudiandae ducimus nihil, assumenda vel ex optio aut
-                voluptates nostrum dolor beatae in voluptatem. Adipisci excepturi
-                nesciunt ab soluta.
-            </div>
-            <br />
-            <div>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea aut a,
-                expedita quaerat, facere iste earum deserunt numquam dignissimos
-                obcaecati voluptatem repellendus nemo eveniet corporis esse voluptatibus
-                facilis laboriosam natus quam sapiente ipsum ipsa, repudiandae qui
-                alias. Ut inventore commodi quae ea excepturi, soluta distinctio
-                laboriosam cum voluptatem consectetur cupiditate neque eius esse placeat
-                consequatur assumenda, unde atque nemo modi perferendis? Saepe
-                molestiae, earum at dicta sit iusto dignissimos vero voluptate voluptas
-                qui ullam molestias omnis cupiditate dolores sunt incidunt ab nisi eaque
-                culpa dolorum? Provident eos doloribus soluta maxime obcaecati quam, a,
-                quod blanditiis dolores odit aliquid quis distinctio.
-            </div>
-            <br />
-            <div>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea aut a,
-                expedita quaerat, facere iste earum deserunt numquam dignissimos
-                obcaecati voluptatem repellendus nemo eveniet corporis esse voluptatibus
-                facilis laboriosam natus quam sapiente ipsum ipsa, repudiandae qui
-                alias. Ut inventore commodi quae ea excepturi, soluta distinctio
-                laboriosam cum voluptatem consectetur cupiditate neque eius esse placeat
-                consequatur assumenda, unde atque nemo modi perferendis? Saepe
-                molestiae, earum at dicta sit iusto dignissimos vero voluptate voluptas
-                qui ullam molestias omnis cupiditate dolores sunt incidunt ab nisi eaque
-                culpa dolorum? Provident eos doloribus soluta maxime obcaecati quam, a,
-                quod blanditiis dolores odit aliquid quis distinctio.
-            </div>
-            <br />
-            <div>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea aut a,
-                expedita quaerat, facere iste earum deserunt numquam dignissimos
-                obcaecati voluptatem repellendus nemo eveniet corporis esse voluptatibus
-                facilis laboriosam natus quam sapiente ipsum ipsa, repudiandae qui
-                alias. Ut inventore commodi quae ea excepturi, soluta distinctio
-                laboriosam cum voluptatem consectetur cupiditate neque eius esse placeat
-                consequatur assumenda, unde atque nemo modi perferendis? Saepe
-                molestiae, earum at dicta sit iusto dignissimos vero voluptate voluptas
-                qui ullam molestias omnis cupiditate dolores sunt incidunt ab nisi eaque
-                culpa dolorum? Provident eos doloribus soluta maxime obcaecati quam, a,
-                quod blanditiis dolores odit aliquid quis distinctio.
-            </div>
-            <br />
-            <div>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea aut a,
-                expedita quaerat, facere iste earum deserunt numquam dignissimos
-                obcaecati voluptatem repellendus nemo eveniet corporis esse voluptatibus
-                facilis laboriosam natus quam sapiente ipsum ipsa, repudiandae qui
-                alias. Ut inventore commodi quae ea excepturi, soluta distinctio
-                laboriosam cum voluptatem consectetur cupiditate neque eius esse placeat
-                consequatur assumenda, unde atque nemo modi perferendis? Saepe
-                molestiae, earum at dicta sit iusto dignissimos vero voluptate voluptas
-                qui ullam molestias omnis cupiditate dolores sunt incidunt ab nisi eaque
-                culpa dolorum? Provident eos doloribus soluta maxime obcaecati quam, a,
-                quod blanditiis dolores odit aliquid quis distinctio.
-            </div>
-            <br />
-            <div>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea aut a,
-                expedita quaerat, facere iste earum deserunt numquam dignissimos
-                obcaecati voluptatem repellendus nemo eveniet corporis esse voluptatibus
-                facilis laboriosam natus quam sapiente ipsum ipsa, repudiandae qui
-                alias. Ut inventore commodi quae ea excepturi, soluta distinctio
-                laboriosam cum voluptatem consectetur cupiditate neque eius esse placeat
-                consequatur assumenda, unde atque nemo modi perferendis? Saepe
-                molestiae, earum at dicta sit iusto dignissimos vero voluptate voluptas
-                qui ullam molestias omnis cupiditate dolores sunt incidunt ab nisi eaque
-                culpa dolorum? Provident eos doloribus soluta maxime obcaecati quam, a,
-                quod blanditiis dolores odit aliquid quis distinctio.
+            <div className={styles.howItWorksContent}>
+                <h1 className={styles.howItWorksTitle}>How it Works</h1>
+
+                <section className={styles.howItWorksSection}>
+                    <p className={styles.howItWorksText}>
+                        The “Digits” game trains users to map estimated quantities onto symbolic numbers, improving arithmetic skills. This practice enhances the accuracy in estimating quantities (without counting individual items) and strengthens the connection between intuitive estimation of quantities and their corresponding symbolic numbers, leading to significant improvements in arithmetic in a short time. The Digits game is effective because it bridges the gap between non-symbolic number sense and symbolic arithmetic, which is critical for developing higher-level math skills.
+                    </p>
+                </section>
+
+                <section className={styles.howItWorksSection}>
+                    <h2 className={styles.howItWorksSubtitle}>Scientific Insights</h2>
+                    <p className={styles.howItWorksText}>
+                        For more scientific insights into how and why the Digits game improves math performance, you can read the full study here:
+                    </p>
+                    <p className={`${styles.howItWorksText} ${styles.citation}`}>
+                        Ferres-Forga, N., Halberda, J., Batalla-Ferres, A., & Bonatti, L. L. (2022). Improving Mathematics Performance in 7-Year-Old Children: Training the Mapping From Estimated Quantities to Arabic Digits. Journal of Numerical Cognition, 8(1), 123-147.
+                        <a href="https://doi.org/10.5964/jnc.8075" target="_blank" className={styles.howItWorksLink}>https://doi.org/10.5964/jnc.8075</a>
+                    </p>
+                </section>
+
+                <section className={styles.howItWorksSection}>
+                    <h2 className={styles.howItWorksSubtitle}>Important Notice</h2>
+                    <p className={styles.howItWorksText}>
+                        It’s important to note that the version of the Digits game used in the scientific study (Ferres-Forga et al., 2022) was a highly restricted version, developed solely to explore whether directly training the mapping between estimated quantities and Arabic digits could improve math proficiency...
+                    </p>
+                </section>
+
+                <section className={styles.howItWorksSection}>
+                    <h2 className={styles.howItWorksSubtitle}>How to Play</h2>
+                    <p className={styles.howItWorksText}>
+                        To see results, it is recommended to play at least two sessions per week for four weeks. However, continued practice will further reinforce the link between estimating quantities and identifying the corresponding Arabic digits, ultimately improving arithmetic abilities.
+                    </p>
+                </section>
+
+                <section className={styles.howItWorksSection}>
+                    <h2 className={styles.howItWorksSubtitle}>Statistics and Progress</h2>
+                    <p className={styles.howItWorksText}>
+                        For players aged 6 and older, it is recommended to start with the standard level and remain in this level for at least four weeks (two sessions per week)...
+                    </p>
+                </section>
+
+                <section className={styles.howItWorksSection}>
+                    <h2 className={styles.howItWorksSubtitle}>Contact Us</h2>
+                    <p className={styles.howItWorksText}>
+                        If you have any questions, you can contact us at <a href="mailto:example@gmail.com" className={styles.howItWorksLink}>example@gmail.com</a>
+                    </p>
+                </section>
             </div>
         </>
     );
-};
+}
 export default Other;
