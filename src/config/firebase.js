18199683e981908fc5979a0fc339d64b90911c1b
AbelBatalla <abelbatallaf@gmail.com>
2024-10-15 23:52:30 +0200

    enviroment variables test

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,14 +6,13 @@ import { getAppCheck, initializeAppCheck, ReCaptchaV3Provider } from "firebase/a
 
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBt4VRbohs2OVISGkOvd5lVoq0xKRofEww",
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "numericalcognition.net",
     projectId: "digits-542ea",
     storageBucket: "digits-542ea.appspot.com",
     messagingSenderId: "41380800132",
-    appId: "1:41380800132:web:71dc1196e406efb49fc4d1",
+    appId: process.env.FIREBASE_APP_ID,
     measurementId: "G-4V10FY4HXB"
 };
 
@@ -23,6 +22,6 @@ export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 const appCheck = initializeAppCheck(app, {
-    provider: new ReCaptchaV3Provider('6LeIRGAqAAAAAMQ7nKlmP7Vzzy282dHdtHIaOjUK'),
+    provider: new ReCaptchaV3Provider(process.env.RECAPTCHA_V3_KEY),
     isTokenAutoRefreshEnabled: true
 });
